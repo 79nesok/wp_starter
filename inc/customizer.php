6d019436a511a1c680f3f9abdea335fa60c1fa47
79nesok <79nesok@gmail.com>
2016-09-27 12:40:25 +0200

    start_wp

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * savacode_starter_theme Theme Customizer.
+ *
+ * @package savacode_starter_theme
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function sst_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'sst_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function sst_customize_preview_js() {
+	wp_enqueue_script( 'sst_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'sst_customize_preview_js' );
